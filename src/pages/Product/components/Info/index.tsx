cd517c455a183ee449434baf86340a7d70a93af7
yanlele <331393627@qq.com>
2022-02-20 21:03:18 +0800

    feat: 个人作品集分类

--- src/pages/Product/components/Info/index.tsx
+++ src/pages/Product/components/Info/index.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react';
+import styles from './style.module.less';
 
 const Info: FC = () => {
   return (
-    <div>
-      <p>作品集</p>
-      <p>个人作品集分类</p>
-      <p>Personal portfolio classification</p>
+    <div className={styles.info}>
+      <p className={styles.line1}>作品集</p>
+      <p className={styles.line2}>个人作品集分类</p>
+      <p className={styles.line3}>Personal portfolio classification</p>
     </div>
   );
 };
--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -2,12 +2,20 @@ import React, { FC } from 'react';
 import Header from '@/components/Header';
 import styles from './style.module.less';
 import BaseNavigate from '@/pages/Product/components/BaseNavigate';
+import Info from '@/pages/Product/components/Info';
 
 const Product: FC = () => {
   return (
     <div className={styles.productContainer}>
       <Header />
-      <BaseNavigate />
+      <img className={styles.portfolio} src="https://cdn.jsdelivr.net/gh/pro-collection/hd/src/static/portfolio.png"
+           alt="" />
+      <div className={styles.content}>
+        <BaseNavigate />
+        <div>
+          <Info />
+        </div>
+      </div>
     </div>
   );
 };
