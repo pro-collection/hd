2ad017838ca6d404ffe93458605d905979723e21
yanlele <331393627@qq.com>
2022-02-20 22:55:20 +0800

    feat: 调整样式

---
+++ src/pages/Product/components/NavTarget/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import styles from './style.module.less';
+
+const NavTarget: FC = () => {
+  return (
+    <div className={styles.navTarget}>
+
+    </div>
+  );
+};
+
+export default NavTarget;
--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -3,6 +3,7 @@ import Header from '@/components/Header';
 import styles from './style.module.less';
 import BaseNavigate from '@/pages/Product/components/BaseNavigate';
 import Info from '@/pages/Product/components/Info';
+import NavTarget from '@/pages/Product/components/NavTarget';
 
 const Product: FC = () => {
   return (
@@ -12,8 +13,9 @@ const Product: FC = () => {
            alt="" />
       <div className={styles.content}>
         <BaseNavigate />
-        <div>
+        <div className={styles.content}>
           <Info />
+          <NavTarget />
         </div>
       </div>
     </div>
