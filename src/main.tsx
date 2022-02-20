d2fc2440d5277f4977a5a38a6c34314e9748f2fb
yanlele <331393627@qq.com>
2022-02-20 13:06:53 +0800

    feat: 添加 router

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import Home from "./pages/Home";
+import Home from "@/pages/Home";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
